dfa1f95dc726d9e5c2fa203905c111c991be0731
Timothy Slippy <timothy.slippy@garmin.com>
2022-12-13 08:39:33 -0800

    starting on day_06

---
+++ src/day_06/day_06.cpp
@@ -0,0 +1,22 @@
+//
+// Created by slippy on 2022-12-02.
+//
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include "aoc.h"
+
+
+using namespace std;
+
+int main() {
+  auto lines = aoc::get_lines("i");
+  auto& datastream = lines.at(0);
+
+  for(size_t i = 4; i < datastream.size(); i++) {
+
+  }
+
+  return EXIT_SUCCESS;
+}
