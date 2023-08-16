7f1218618417fe0732c877b5ccc3998e906d7714
Timothy Slippy <timothy.slippy@garmin.com>
2023-08-16 09:59:43 -0700

    day_06 complete

--- src/day_06/day_06.cpp
+++ src/day_06/day_06.cpp
@@ -7,16 +7,42 @@
 #include <sstream>
 #include "aoc.h"
 
+#define PACKET_MARKER_SIZE (4)
+#define MESSAGE_MARKER_SIZE (14)
 
 using namespace std;
+
+size_t marker_position(int marker_size, const string &datastream) {
+  size_t position = 3;
+  for(; position < datastream.size(); position++) {
+    array<bool, 26>found{};
+    found[datastream.at(position) - 'a'] = true;
+    bool is_marker = true;
+    for(size_t j = 1; j < marker_size; j++) {
+      size_t idx = position - j;
+      if(found[datastream.at(idx) - 'a']) {
+        is_marker = false;
+        break;
+      }
+      else {
+        found[datastream.at(idx) - 'a'] = true;
+      }
+    }
+
+    if(is_marker) {
+      break;
+    }
+  }
+  return position + 1;
+}
+
 
 int main() {
   auto lines = aoc::get_lines("i");
   auto& datastream = lines.at(0);
 
-  for(size_t i = 4; i < datastream.size(); i++) {
-
-  }
+  cout << "PACKET MARKER: " << marker_position(PACKET_MARKER_SIZE, datastream) << endl;
+  cout << "MESSAGE MARKER: " << marker_position(MESSAGE_MARKER_SIZE, datastream) << endl;
 
   return EXIT_SUCCESS;
 }
